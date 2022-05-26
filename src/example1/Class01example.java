27b9fabed4f81ec1ef254450872cb06eaf35f2f5
OlegSheremetyev <olegsheremetyev@gmail.com>
2022-05-26 17:55:43 -0400

    2nd commit

--- src/example1/Class01example.java
+++ src/example1/Class01example.java
@@ -3,6 +3,8 @@ package example1;
 public class Class01example {public static void main(String[] args) {
 	
 	System.out.println("hello batch13");
+	
+	System.out.println("Toss a coin to a witcher");
 }
 
 }
