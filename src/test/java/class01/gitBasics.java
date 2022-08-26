dace0d80ce2ffa374cceb2bbc4ff450a17de1f16
OlegSheremetyev <olegsheremetyev@gmail.com>
2022-08-26 19:10:36 -0400

    temperature1

--- src/test/java/class01/gitBasics.java
+++ src/test/java/class01/gitBasics.java
@@ -2,9 +2,9 @@ package class01;
 
 public class gitBasics {
     public static void main(String[] args) {
- 
+
        // write down the code that checks temperature
         //prints out if it's a beautiful day or not
-        System.out.println("today is a beautiful day");
+        System.out.println("today is not a beautiful day");
     }
 }
